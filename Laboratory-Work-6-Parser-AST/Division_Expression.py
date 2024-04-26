ba98c3aa88dfde63a1a3a4ef74302c6c830c10ba
Ghenntoggy1 <roman.gusev@isa.utm.md>
2024-04-27 00:43:19 +0300

    return

--- Laboratory-Work-6-Parser-AST/Division_Expression.py
+++
@@ -1,8 +0,0 @@
-from Node import BinaryNode
-from Exponential_Expression import Exponential_Expression
-
-
-class Division_Expression(BinaryNode):
-    @classmethod
-    def construct(cls, parser):
-        return cls.construct_binary(parser, cls, Exponential_Expression, ["/"])
--- Laboratory-Work-6-Parser-AST/Multiplicative_Expression.py
+++ Laboratory-Work-6-Parser-AST/Multiplicative_Expression.py
@@ -1,16 +1,8 @@
 from Node import BinaryNode
 from Exponential_Expression import Exponential_Expression
-from Division_Expression import Division_Expression
 
 
 class Multiplicative_Expression(BinaryNode):
     @classmethod
     def construct(cls, parser):
-        node = Division_Expression.construct(parser)
-
-        if not parser.next().has("*"):
-            return node
-
-        op = parser.take()
-        right = Multiplicative_Expression.construct(parser)
-        return Multiplicative_Expression(node, op, right)
+        return cls.construct_binary(parser, cls, Exponential_Expression, ["*", "/"])
