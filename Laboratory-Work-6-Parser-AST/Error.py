b27d913b8692ca6652e3262741525039ccc6b32d
Ghenntoggy1 <roman.gusev@isa.utm.md>
2024-04-26 11:51:04 +0300

    som

--- Laboratory-Work-6-Parser-AST/Error.py
+++
@@ -1,8 +0,0 @@
-class LanguageError(RuntimeError):
-    def __init__(self, component, message):
-        component.mark()
-        self.line = component.line
-        self.message = message
-
-    def __str__(self):
-        return f"{self.line.get_marks()}\n{type(self).__name__} : {self.message}"
