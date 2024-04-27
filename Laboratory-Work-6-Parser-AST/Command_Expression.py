f0ba87cb941a407b0149335a3886589d185d5df5
Ghenntoggy1 <roman.gusev@isa.utm.md>
2024-04-27 15:21:51 +0300

    feature: Add parsing for Comment Block

--- Laboratory-Work-6-Parser-AST/Command_Expression.py
+++ Laboratory-Work-6-Parser-AST/Command_Expression.py
@@ -12,14 +12,39 @@ class Command_Expression(Node):
 
     @classmethod
     def construct(cls, parser):
-        if parser.next().of("COMMENT_LINE"):
-            return Comment_Line_Expression(parser.take())
+        if parser.next().of("COMMENT_LINE", "COMMENT_BLOCK"):
+            return Comment_Expression(parser.take())
 
         command = Variable_Expression.construct(parser)
 
         right = parser.expecting_has(";")
 
         return Command_Expression(command, right)
+
+class Comment_Expression(Node):
+    def __init__(self, comment):
+        self.comment = comment
+
+    def nodes(self):
+        return [self.comment]
+
+    @classmethod
+    def construct(cls, parser):
+        if parser.next().of("COMMENT_LINE"):
+            return Comment_Line_Expression(parser)
+        else:
+            return Comment_Block_Expression(parser)
+
+class Comment_Block_Expression(Node):
+    def __init__(self, comment_line):
+        self.comment_line = comment_line
+
+    def nodes(self):
+        return [self.comment_line]
+
+    @classmethod
+    def construct(cls, parser):
+        return Comment_Block_Expression(parser.expecting_of("COMMENT_BLOCK"))
 
 class Comment_Line_Expression(Node):
     def __init__(self, comment_line):
